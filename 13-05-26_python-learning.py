60d4cb0783571292d3d51f92d2d1c9c097646b37
Raj Jivani <raj.developer0112@gmail.com>
2026-05-15 10:08:24 +0530

    daily practice

---
+++ 13-05-26_python-learning.py
@@ -0,0 +1,23 @@
+# Today's learning
+
+# Functions: 
+    # 1. Built-in-functions
+    # 2. UDF (user define function)
+
+
+print("Hello World")
+
+name = input("Entre your name: ")  # we use input() to take input from user
+print(name , ", Welcome to learning of python")
+
+a = int(input("Entre value of A: "))  # here we use type casting because whenever user input something it comes in form of string
+b = int(input("Entre value of B: "))
+
+print("sum of a & b is: ", a + b)
+print("sub of a & b is: ", a - b)
+print("mul of a & b is: ", a * b)
+print("div of a & b is: ", a / b)
+
+print(type(a))  # using type() we can check data type 
+print(type(b))
+
